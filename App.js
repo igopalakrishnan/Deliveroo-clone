e8d40f25f9f41370770d362a169de92da3f57cf9
igopalakrishnan <kingkrishna809@gmail.com>
2022-09-21 12:47:49 +0530

    create OrderScreen with animation

--- App.js
+++ App.js
@@ -17,17 +17,22 @@ export default function App() {
   return (
     <NavigationContainer>
       <Provider store={store}>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-        <Stack.Screen name="Basket" 
-        component={BasketScreen}
-        options={{ presentation: "modal", headerShown: false}}
-         />
-         <Stack.Screen 
-         name="PreparingOrderScreen" component={PreparingOrderScreen}
-         options={{ presentation: "fullScreenModal", headerShown: false}} />
-      </Stack.Navigator>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+          <Stack.Screen name="Basket"
+            component={BasketScreen}
+            options={{ presentation: "modal", headerShown: false }}
+          />
+          <Stack.Screen
+            name="PreparingOrderScreen"
+            component={PreparingOrderScreen}
+            options={{ presentation: "fullScreenModal", headerShown: false }} />
+          <Stack.Screen
+            name="DeliveryScreen"
+            component={DeliveryScreen}
+            options={{ presentation: "fullScreenModal", headerShown: false }} />
+        </Stack.Navigator>
       </Provider>
     </NavigationContainer>
   );
